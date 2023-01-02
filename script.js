e6115fcfcd8cffb1d8e8685d9648837d05e94572
lat1e <lat1e.cafe@gmail.com>
2023-01-02 21:20:37 +0900

    update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // {id:1, content:"할일", done:false}
-const todoData = [];
+let todoData = [];
 let lastId = 0;
+
+if (localStorage.getItem("todoData")) {
+    todoData = JSON.parse(localStorage.getItem("todoData"));
+    lastId = JSON.parse(localStorage.getItem("lastId"));
+}
 
 // add
 function addTodoItem(content) {
@@ -10,6 +15,7 @@ function addTodoItem(content) {
         done: false
     });
     lastId++;
+    actionAfterUpdateTodo();
 }
 
 // delete
@@ -19,17 +25,33 @@ function deleteTodoItem(id) {
             todoData.splice(i, 1);
         }
     }
+    actionAfterUpdateTodo();
 }
 
 // done
-function makeDoneTodoItem(id) {
-
+function updateDoneTodoItem(id, done) {
+    for (const i in todoData){
+        if (id === todoData[i].id) {
+            todoData[i].done = done;
+        }
+    }
+    actionAfterUpdateTodo();
 }
 
-// test
+function actionAfterUpdateTodo() {
+    saveTodoData()
+}
+
+function saveTodoData() {
+    localStorage.setItem("todoData", JSON.stringify(todoData));
+    localStorage.setItem("lastId", JSON.stringify(lastId));
+}
+
+/* test
 addTodoItem("abc");
 addTodoItem("bcd");
 addTodoItem("cdf");
 console.log(JSON.stringify(todoData));
 deleteTodoItem(1);
 console.log(todoData);
+updateDoneTodoItem(0, true); */
